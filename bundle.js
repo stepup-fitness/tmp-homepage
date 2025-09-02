06b4dc04c09628c6d4f2dd8a97f2c76932897d9f
stepup-fitness <matteodifraia@stepup-fitness.com>
2025-09-02 11:01:21 +0200

    Add files via upload

---
+++ bundle.js
@@ -0,0 +1,4 @@
+// StepUp bundle placeholder
+// Qui andrebbe il codice React compilato con Babel/Webpack.
+// Per esempio: ReactDOM.createRoot(document.getElementById('root')).render(React.createElement(StepUpHomePage));
+console.log("StepUp bundle loaded");
